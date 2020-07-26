3523d1fa2a5a10a01a5a1010ff6a9ff7a7ebca64
Shivam Chauhan <20shivamchauhan@gmail.com>
2020-07-26 22:08:56 +0530

    added monaco editor and jstree logic

--- VS_Code/index.js
+++ VS_Code/index.js
@@ -3,45 +3,75 @@ const path = require("path");
 const fs = require("fs");
 require("jstree");
 
-$(document).ready(function(){
+$(document).ready(function () {
 
     let pPath = process.cwd();
-    let pName = path.basename(pPath);
-    
-    let data = [{
-        id : pPath,
-        parent : "#",
-        text : pName
-    }];
+    let name = path.basename(pPath);
 
-    let childArr = createChildData(pPath);
-    data = [...data,...childArr];
+    let data = [{
+        id: pPath,
+        parent: "#",
+        text: name
+    }]
+    let childArr = addCh(pPath);
+    data = [...data, ...childArr];
+
+    const amdLoader = require('./node_modules/monaco-editor/min/vs/loader.js');
+    const amdRequire = amdLoader.require;
+    const amdDefine = amdLoader.require.define;
+
+    amdRequire.config({
+        baseUrl: './node_modules/monaco-editor/min'
+    });
+
+    // workaround monaco-css not understanding the environment
+    self.module = undefined;
+
+    amdRequire(['vs/editor/editor.main'], function () {
+        var editor = monaco.editor.create(document.getElementById('editor'), {
+            value: [
+                'function x() {',
+                '\tconsole.log("Hello world!");',
+                '}'
+            ].join('\n'),
+            language: 'javascript'
+        });
+    });
 
     $('#tree').jstree({
-        "core" : {
-            "check_callback" : true,
-            "data" : data
+        "core": {
+            "check_callback": true,
+            "data": data
         }
-    }).on("select_node.jstree",function(e,data){
-        let childP = data.node.id;
-        let childArr = createChildData(childP);
-        for(let i = 0 ; i < childArr.length ; i++){
-            $('#tree').jstree().create_node(childP ,childArr[i], "last");
+    }).on("open_node.jstree", function (e, data) {
+        let children = data.node.children;
+        for (let i = 0; i < children.length; i++) {
+            let gcArr = addCh(children[i]);
+            for (let j = 0; j < gcArr.length; j++) {
+                let doesExist = $('#tree').jstree(true).get_node(gcArr[j].id);
+                if (doesExist)
+                    return;
+                $("#tree").jstree().create_node(children[i], gcArr[j], "last");
+            }
         }
     })
 })
 
-function createChildData(pPath){
-    let childrens = fs.readdirSync(pPath);
-    let cData = [];
-    for(let i = 0 ; i < childrens.length ; i++){
-        let obj = {
-            id : path.join(pPath,childrens[i]),
-            parent : pPath,
-            text : childrens[i]
-        }
-        cData.push(obj);
+function addCh(parentPath) {
+    let isDir = fs.lstatSync(parentPath).isDirectory();
+    if (!isDir) {
+        return [];
     }
-
-    return cData;
+    let childrens = fs.readdirSync(parentPath);
+    let cdata = [];
+    for (let i = 0; i < childrens.length; i++) {
+        let cPath = path.join(parentPath, childrens[i]);
+        let obj = {
+            id: cPath,
+            parent: parentPath,
+            text: childrens[i]
+        };
+        cdata.push(obj);
+    }
+    return cdata;
 }
