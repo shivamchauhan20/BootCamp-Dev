006c0c4ce05aa2186e793ae4005825dd3f0aef77
Shivam Chauhan <20shivamchauhan@gmail.com>
2020-08-13 22:07:14 +0530

    added toolbar options

--- OpenBoard/script.js
+++ OpenBoard/script.js
@@ -1,12 +1,21 @@
 let isPenDown = false;
-ctx.lineWidth = 15;
+let points = [];
+let redoArr = [];
 board.addEventListener("mousedown", function (e) {
     let top = getPosition();
     let x = e.clientX;
     let y = e.clientY - top;
-    ctx.beginPath(0,0);
+    ctx.beginPath(0, 0);
     ctx.moveTo(x, y);
     isPenDown = true;
+    let mdo = {
+        x: x,
+        y: y,
+        id: "md",
+        color: ctx.strokeStyle,
+        width: ctx.lineWidth
+    }
+    points.push(mdo);
 })
 board.addEventListener("mousemove", function (e) {
     if (isPenDown) {
@@ -15,12 +24,63 @@ board.addEventListener("mousemove", function (e) {
         let y = e.clientY - top;
         ctx.lineTo(x, y);
         ctx.stroke();
+        let mmo = {
+            x: x,
+            y: y,
+            id: "mm",
+            color: ctx.strokeStyle,
+            width: ctx.lineWidth
+        }
+        points.push(mmo);
     }
 })
-board.addEventListener("mouseup",function(){
+board.addEventListener("mouseup", function () {
     isPenDown = false;
 })
 
-function getPosition(){
+function getPosition() {
     return board.getBoundingClientRect().top;
+}
+
+function redraw() {
+    for (let i = 0; i < points.length; i++) {
+        if (points[i].id == "md") {
+            ctx.beginPath();
+            ctx.moveTo(points[i].x, points[i].y);
+            ctx.strokeStyle = points[i].color;
+            ctx.lineWidth = points[i].width;
+        }
+        else {
+            ctx.lineTo(points[i].x, points[i].y);
+            ctx.stroke();
+        }
+    }
+}
+
+function undoMaker() {
+    if (points.length >= 2) {
+        let tempArr = [];
+        for (let i = points.length - 1; i >= 0; i--) {
+            if (points[i].id == "md") {
+                tempArr.unshift(points.pop());
+                break;
+            }
+            tempArr.unshift(points.pop());
+            
+        }
+        redoArr.push(tempArr);
+        ctx.clearRect(0, 0, board.width, board.height);
+        redraw();
+    }
+}
+
+function redoMaker() {
+    if(redoArr.length > 0){
+        let tempArr = redoArr.pop();
+        for(let i = 0 ; i < tempArr.length ; i++){
+            points.push(tempArr[i]);
+        }
+        ctx.clearRect(0,0,board.width,board.height);
+        redraw();
+    }
 }
--- OpenBoard/tools.js
+++ OpenBoard/tools.js
@@ -1,9 +1,52 @@
 let pencil = document.querySelector("#pencil");
 let eraser = document.querySelector("#eraser");
-
-pencil.addEventListener("click",function(){
-    ctx.strokeStyle = "black";
+let undo = document.querySelector("#undo");
+let redo = document.querySelector("#redo");
+let pencilOptions = document.querySelector("#pencil-options");
+let eraserOptions = document.querySelector("#eraser-options");
+let slider = document.querySelector("#range");
+let activeClass = "pencil";
+pencil.addEventListener("click", function () {
+    if (activeClass == "pencil") {
+        pencilOptions.classList.add("show");
+    }
+    else {
+        activeClass = "pencil";
+        eraserOptions.classList.remove("show");
+        ctx.strokeStyle = "black";
+    }
 });
-eraser.addEventListener("click",function(){
-    ctx.strokeStyle = "white";
+eraser.addEventListener("click", function () {
+    if (activeClass == "eraser") {
+        eraserOptions.classList.add("show");
+    }
+    else {
+        activeClass = "eraser";
+        pencilOptions.classList.remove("show");
+        ctx.strokeStyle = "white";
+    }
+});
+undo.addEventListener("click", function () {
+    undoMaker();
+});
+redo.addEventListener("click", function () {
+    redoMaker();
 })
+
+document.addEventListener("keydown", function (e) {
+    var evtobj = e
+    if (evtobj.keyCode == 90 && evtobj.ctrlKey) {
+        undoMaker();
+    }
+    if (evtobj.keyCode == 89 && evtobj.ctrlKey) {
+        redoMaker();
+    }
+});
+
+function handleColor(color){
+    ctx.strokeStyle = color;
+}
+
+function handleSlider(){
+    ctx.lineWidth = slider.value;
+}
